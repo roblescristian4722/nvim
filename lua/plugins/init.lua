44f4e70a63a8cfb7804ba384b676ff4ffafb3943
roblescristian4722 <roblescristian4722@gmail.com>
2023-10-17 00:29:52 +0000

    Adding multiple cursor keymaps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,7 +39,16 @@ local plugins = {
   },
 
   -- Multiple cursors
-  "mg979/vim-visual-multi",
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",
+        ['Find Subword Under'] = '<C-d>'
+      }
+    end,
+  },
 
   -- Auto pairs for tags
   'alvan/vim-closetag',
