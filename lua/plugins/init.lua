ad9f633bb395a89a7360e8fdc10a8331cd280df7
roblescristian4722 <roblescristian4722@gmail.com>
2023-12-18 19:34:48 +0000

    Improving multiple cursors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -43,9 +43,11 @@ local plugins = {
     "mg979/vim-visual-multi",
     branch = "master",
     init = function()
+      vim.g.VM_mouse_mappings = 1
       vim.g.VM_maps = {
-        ["Find Under"] = "<C-d>",
-        ['Find Subword Under'] = '<C-d>'
+        ['Find Under'] = "<C-d>",
+        ['Find Subword Under'] = '<C-d>',
+        ['Select All'] = '<space>a',
       }
     end,
   },
