4a79a717f498aaf9f319d947143f4725103d949c
roblescristian4722 <roblescristian4722@gmail.com>
2024-04-02 02:11:50 -0600

    DAP working a little better (not entirely)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,7 +199,6 @@ local plugins = {
   "rcarriga/nvim-dap-ui",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "jay-babu/mason-nvim-dap.nvim",
       "theHamsta/nvim-dap-virtual-text",
       "anuvyklack/hydra.nvim",
       "nvim-telescope/telescope-dap.nvim",
@@ -209,24 +208,36 @@ local plugins = {
     config = function ()
       local dap = require("dap")
       local dapui = require("dapui")
-      require("mason-nvim-dap").setup({
-        automatic_setup = true,
-        ensure_installed = {
-          "cpptools",
-          "codelldb"
-        },
-      })
-
-      dap.listeners.after.event_initialized['dapui_config'] = function()
+      dap.listeners.before.attach.dapui_config = function()
         dapui.open()
       end
-      dap.listeners.before.event_terminated['dapui_config'] = function()
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
       end
-      dap.listeners.before.event_exited['dapui_config'] = function()
+      dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
     end
+  },
+
+  -- Mason DAP
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      'williamboman/mason.nvim',
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "cpptools",
+        "codelldb"
+      }
+    }
   },
 
   -- Custom notifications
